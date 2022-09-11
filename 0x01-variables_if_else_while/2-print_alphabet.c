eb3db62af6d5928fb41285305bccd8d040a7ff57
OgboiIsrael <israel.imoleoluwa.kemi@gmail.com>
2022-09-11 06:14:47 -0700

    push my correction on 2-print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,12 +10,13 @@ int main(void)
 {
 	char b;
 
-  b = 'a';
-  while (b <= 'z')
-  {
-    putchar(b);
-    b++;
-  }
-  putchar('\n');
-  return (0);
+	b = 'a';
+
+	while (b <= 'z')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
 }
