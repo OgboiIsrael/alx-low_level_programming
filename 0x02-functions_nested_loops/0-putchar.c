75bf06b593a60fcee13aa9d1de364cd7b198b283
OgboiIsrael <israel.imoleoluwa.kemi@gmail.com>
2022-09-14 06:07:15 -0700

    print putchar using putchar-correction2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,10 +11,10 @@ int main(void)
 	char prnt[] = "_putchar";
 	int wrd;
 
-	for(wrd = 0; wrd < 8; wrd++)
+	for (wrd = 0; wrd < 8; wrd++)
 	{
 		putchar(prnt[wrd]);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
