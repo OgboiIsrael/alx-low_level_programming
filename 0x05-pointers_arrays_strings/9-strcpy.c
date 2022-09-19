c2e49c60aed1f0e25d608139918c648df870106a
OgboiIsrael <israel.imoleoluwa.kemi@gmail.com>
2022-09-19 15:09:54 -0700

    copies the string pointed to by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src, including \0.
+ *@src: pointer.
+ *@dest: pointer.
+ *Return: the pointer to dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, len;
+
+	for (len = 0; src[len] != '\0'; len++)
+		;
+
+	for (i = 0; i <= len; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
